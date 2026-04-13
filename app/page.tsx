612611252d67f082a25ebc65f1788a4c107d7f7d
JoshTeflon <jenikele21h@gmail.com>
2026-04-13 22:48:24 +0100

    main/feat: setup navigation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Navigation } from "@/components";
+
 export default function Home() {
   return (
-    <div className="">
+    <div className="p-8 h-screen flex flex-col justify-between">
       <main className=""></main>
+
+      <header className="mt-auto">
+        <Navigation />
+      </header>
     </div>
   );
 }
---
+++ components/navigation.tsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+    <nav className="p-2 w-full flex items-center justify-between uppercase">
+      <h1 className="text-base tracking-wide font-nosifer">joshua.enikele</h1>
+
+      <ul className="flex items-center space-x-16 text-sm">
+        <li>home</li>
+        <li>work</li>
+        <li>contact</li>
+      </ul>
+
+      <div className="text-sm">Lagos, NGA: (GMT+1) 19:45</div>
+    </nav>
+  )
+}
+
+export default Navigation
