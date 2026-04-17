428c78b737970056406e445b04ebd51de9166a59
JoshTeflon <jenikele21h@gmail.com>
2026-04-17 23:38:20 +0100

    topbar-ui/feat: update icons to social links

--- components/ui/top-bar.tsx
+++ components/ui/top-bar.tsx
@@ -7,8 +7,22 @@ const TopBar = () => {
     <div className="p-2 w-full max-h-8 bg-foreground text-background flex items-center justify-between">
       <h1 className="text-sm font-medium uppercase">{me.role}</h1>
       <div className="flex items-center space-x-2 xl:space-x-3">
-        <GithubIcon />
-        <LinkedinIcon />
+        <a
+          href={me.socials.github}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit Joshua Enikele's GitHub profile"
+        >
+          <GithubIcon />
+        </a>
+        <a
+          href={me.socials.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit Joshua Enikele's LinkedIn profile"
+        >
+          <LinkedinIcon />
+        </a>
       </div>
     </div>
   );
