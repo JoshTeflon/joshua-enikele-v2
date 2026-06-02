ca71f4c6950be5bb371e135ce4c1413b59c33d1e
JoshTeflon <jenikele21h@gmail.com>
2026-06-02 22:13:51 +0100

    style: adjust typography and layout in Hero component for improved readability and alignment

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -6,21 +6,21 @@ const Hero = () => {
   return (
     <section className="flex flex-1 flex-col justify-center gap-8">
       <div className="flex flex-col gap-6 lg:flex-row lg:items-start lg:justify-between lg:gap-12">
-        <h2 className="self-start whitespace-nowrap font-duvel uppercase leading-[0.8] tracking-widest text-[clamp(4rem,21vw,16rem)] lg:text-[clamp(3.25rem,17vw,16rem)]">
+        <h2 className="self-start whitespace-nowrap font-duvel uppercase leading-[0.75] tracking-widest text-[clamp(4rem,21vw,16rem)] lg:text-[clamp(3.25rem,17vw,16rem)]">
           {firstName}
         </h2>
 
-        <p className="max-w-[20rem] self-start text-[1rem] uppercase leading-snug tracking-[0.05em] sm:max-w-[24rem] lg:mt-6">
+        <p className="max-w-96 self-start text-sm lg:text-base uppercase leading-snug tracking-[0.05em]">
           {tagline}
         </p>
       </div>
 
-      <div className="flex flex-col gap-6 lg:flex-row lg:items-end lg:justify-between lg:gap-12">
-        <p className="max-w-[24rem] text-[1rem] uppercase leading-snug tracking-[0.05em]">
+      <div className="flex flex-col-reverse gap-6 lg:flex-row lg:items-end lg:justify-between lg:gap-12">
+        <p className="max-w-96 self-end text-right text-sm lg:self-auto lg:text-left lg:text-base uppercase leading-snug tracking-[0.05em]">
           {tagline}
         </p>
 
-        <h2 className="self-end whitespace-nowrap text-right font-duvel uppercase leading-[0.8] tracking-widest text-[clamp(4rem,21vw,16rem)] lg:text-[clamp(3.25rem,17vw,16rem)]">
+        <h2 className="self-end whitespace-nowrap text-right font-duvel uppercase leading-[0.75] tracking-widest text-[clamp(4rem,21vw,16rem)] lg:text-[clamp(3.25rem,17vw,16rem)]">
           {lastName}
         </h2>
       </div>
