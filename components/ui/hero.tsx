d309d2168f5e6918284cbdf7643b286925212f09
JoshTeflon <jenikele21h@gmail.com>
2026-06-02 22:20:07 +0100

    style: refine layout and alignment in Hero component for better visual consistency

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -10,17 +10,17 @@ const Hero = () => {
           {firstName}
         </h2>
 
-        <p className="max-w-96 self-start text-sm lg:text-base uppercase leading-snug tracking-[0.05em]">
+        <p className="max-w-96 self-start lg:self-auto text-sm lg:text-base uppercase leading-snug tracking-[0.05em]">
           {tagline}
         </p>
       </div>
 
-      <div className="flex flex-col-reverse gap-6 lg:flex-row lg:items-end lg:justify-between lg:gap-12">
+      <div className="flex flex-col-reverse gap-6 lg:flex-row lg:items-baseline-last lg:justify-between lg:gap-12">
         <p className="max-w-96 self-end text-right text-sm lg:self-auto lg:text-left lg:text-base uppercase leading-snug tracking-[0.05em]">
           {tagline}
         </p>
 
-        <h2 className="self-end whitespace-nowrap text-right font-duvel uppercase leading-[0.75] tracking-widest text-[clamp(4rem,21vw,16rem)] lg:text-[clamp(3.25rem,17vw,16rem)]">
+        <h2 className="self-end lg:self-auto whitespace-nowrap text-right font-duvel uppercase leading-[0.75] tracking-widest text-[clamp(4rem,21vw,16rem)] lg:text-[clamp(3.25rem,17vw,16rem)]">
           {lastName}
         </h2>
       </div>
