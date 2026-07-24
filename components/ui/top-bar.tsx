bfaaaeddb27aaa7c26155a0c217059541b487bb7
JoshTeflon <jenikele21h@gmail.com>
2026-07-24 10:17:13 +0100

    refactor: simplify Navigation component by removing location display and adjusting layout; enhance TopBar with dynamic time display

--- components/ui/top-bar.tsx
+++ components/ui/top-bar.tsx
@@ -1,13 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import { GithubIcon, LinkedinIcon } from "../icons";
 
+import { getLagosTime } from "@/lib/utils";
 import ThemeToggle from "./theme-toggle";
 
 import me from "@/content/me.json";
 
 const TopBar = () => {
+  const { location } = me;
+
+  const [time, setTime] = useState(getLagosTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(getLagosTime()), 1000)
+    return () => clearInterval(interval)
+  }, []);
+  
   return (
     <header className="fixed inset-x-6 top-6 xl:inset-x-8 xl:top-8 z-50 p-2 max-h-8 bg-foreground text-background flex items-center justify-between">
-      <h1 className="text-base font-light uppercase tracking-widest">{me.role}</h1>
+      <div className="text-sm uppercase">
+        {location.city}, {location.country}: ({location.timezone}) {time}
+      </div>
 
       <div className="flex items-center space-x-2 xl:space-x-3">
         <a
